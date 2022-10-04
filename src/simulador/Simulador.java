063600db0c36e7a03974d138bef2dd4d8696099e
Fozzy3 <carlmora027@gmail.com>
2022-10-04 00:38:15 -0500

    Subida

---
+++ src/simulador/Simulador.java
@@ -0,0 +1,13 @@
+package simulador;
+
+import vista.Menu;
+
+public class Simulador {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Menu menu = new Menu();
+        menu.setVisible(true);
+    }
+    
+}
